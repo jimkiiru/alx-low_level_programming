9fbe13962710782d4c30fee36c8b976dcb480083
jimkiiru <kiiru.kurj@gmail.com>
2023-03-17 13:49:50 -0700

    alphabet in lowercase: except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: alphabet in lowercase
+* @void:
+*
+* Return: returns zero after succesfull execution
+*/
+int main(void)
+{
+
+	int i;
+
+	for (i = 97; i <=122; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
