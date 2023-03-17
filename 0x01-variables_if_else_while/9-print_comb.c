859c252645ec725eb2655e6e73385eab7334e0dd
jimkiiru <kiiru.kurj@gmail.com>
2023-03-17 15:20:29 -0700

    single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,37 +11,18 @@
 int main(void)
 {
 
-	int i = 0, j = 0, k = 0, l = 0;
+	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
-		for (j = i + 1; j < 10; j++)
+		putchar(i);
+		if (i < 57)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(',');
-			putchar(' ');
-			for (k = j + 1; k < 10; k++)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(k + '0');
-				putchar(',');
-				putchar(' ');
-				for (l = k + 1; l < 10; l++)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
-					putchar(l + '0');
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar(44);
+			putchar(32);
 		}
 	}
-	return 0;
+	putchar('\n');
+
+	return (0);
 }
