41631a18d1bfa87e0eae5f56d6213c1752c1a8a2
jimkiiru <kiiru.kurj@gmail.com>
2023-03-17 13:40:31 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: alphABET
+* @void:
+*
+* Return: returns zero after succesfull execution
+*/
+int main(void){
+
+	int i;
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 65; i <= 90; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return 0;
+
+}
