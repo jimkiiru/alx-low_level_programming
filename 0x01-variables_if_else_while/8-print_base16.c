f71d47f95d3a50d7e8d8c1425bd95e5290218891
jimkiiru <kiiru.kurj@gmail.com>
2023-03-17 14:18:21 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* 
+* Description: Hexadecimal
+* @void: 
+* 
+* Return: returns zero after succesfull execution
+*/
+int main() {
+    
+    int i;
+
+    for (i = 0; i < 16; i++)
+    {
+        if (i < 10)
+	{
+            putchar(i + '0');
+        } else
+	{
+            putchar(i - 10 + 'a');
+        }
+    }
+
+    putchar('\n');
+
+    return (0);
+}
