5bac8287dcd907fb91fa09b46f0855399f57256d
jimkiiru <kiiru.kurj@gmail.com>
2023-03-17 15:07:31 -0700

    fix bud

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,37 +11,37 @@
 int main(void)
 {
 
-	int i, j, k, l;
+	int i = 0, j = 0, k = 0, l = 0;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(i + 48);
+		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
-		for (j = i+1; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			putchar(i + 48);
-			putchar(j + 48);
+			putchar(i + '0');
+			putchar(j + '0');
 			putchar(',');
 			putchar(' ');
-			for (k = j+1; k < 10; k++)
+			for (k = j + 1; k < 10; k++)
 			{
-				putchar(i + 48);
-				putchar(j + 48);
-				putchar(k + 48);
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 				putchar(',');
 				putchar(' ');
-				for (l = k+1; l < 10; l++)
+				for (l = k + 1; l < 10; l++)
 				{
-					putchar(i + 48);
-					putchar(j + 48);
-					putchar(k + 48);
-					putchar(l + 48);
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					putchar(l + '0');
 					putchar(',');
 					putchar(' ');
 				}
 			}
 		}
 	}
-	return (0);
+	return 0;
 }
