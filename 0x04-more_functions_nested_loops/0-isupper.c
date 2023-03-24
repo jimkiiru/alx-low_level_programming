8e5f75d3d7588bdcb491b22aab480d6706a988e0
jimkiiru <kiiru.kurj@gmail.com>
2023-03-24 12:20:33 -0700

    ident 0-isupper.c class

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,20 +7,23 @@
  */
 int main(void)
 {
-    char c;
+	char c;
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
 
-int _isupper(int c) {
-    if (c >= 'A' && c <= 'Z') {
-        return 1;
-    }
-    else {
-        return 0;
-    }
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
